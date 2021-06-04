0eaa8cbbfea03409606971abf3fcfba0ed6d3c0f
Manish Tiwari <mtiwari.t009@gmail.com>
2021-06-04 12:21:28 +0530

    GitHub1 Commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 // import loginImg from '../images/login-img.svg';
-import loginImg from '../images/login-img.svg'
+import loginImg from '../images/login1.svg'
 const Login = () => {
 
   const{loginWithRedirect} = useAuth0();
   return <Wrapper>
     <div className="container">
       <img src={loginImg} alt="" />
-      <h1>Login Page</h1>
+      <h1 style={{textShadow:"2px 2px 4px #000000"}}>Login Page</h1>
       <button className="btn" onClick={loginWithRedirect}>Login / sign up</button>
     </div>
   </Wrapper>
@@ -18,6 +18,7 @@ const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
+  background: linear-gradient(to bottom right, #3399ff 0%, #ffffff 100%);
   .container {
     width: 90vw;
     max-width: 600px;
